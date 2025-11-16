fd794d1b27c0cdcf2f2e4e0a568cc17748b46b88
Anthony Villarreal <anthonyvml123@hotmail.es>
2025-11-15 21:12:02 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Dominio de Railway frontend
+const ALLOWED_HOST = "daring-warmth-production.up.railway.app";
+
 export default defineConfig({
   plugins: [react()],
 
+  // Usado sólo en desarrollo (vite dev)
   server: {
-    host: '0.0.0.0',
+    host: true,
     port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    },
+    allowedHosts: [ALLOWED_HOST],
   },
 
+  // Usado en Railway (vite preview)
   preview: {
-    host: '0.0.0.0',
+    host: true,
     port: 8080,
-    allowedHosts: ['https://chat-espe-frontend-production.up.railway.app/'],
+    allowedHosts: [ALLOWED_HOST],
   },
 })
