7de256539423e6d48d260b87a5a327e1e77673d3
Anthony Villarreal <anthonyvml123@hotmail.es>
2025-11-15 20:25:52 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',  // ← Permite acceso desde LAN
+    host: '0.0.0.0',
     port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    },
   },
+  preview: {
+    host: '0.0.0.0',
+    port: 8080,
+    allowedHosts: ['chat-espe-frontend-production.up.railway.app']
+  },
+  build: {
+    outDir: 'dist',
+  }
 })
